945b81645807bba9e90555045e8f6b4667bf21bd
이재호 <jhlee@ibleaders.co.kr>
2021-09-19 18:48:19 +0900

    update loader config, problem: path

--- samples/src/components/App.js
+++ samples/src/components/App.js
@@ -1,25 +1,7 @@
 // Main.
 import Layout from 'components/Layout';
-import IBSheetLoader from '@ibsheet/loader';
 
 const App = () => {
-
-  // 로더 config
-  IBSheetLoader.config({
-    registry: [
-      {
-        name: 'ibsheet',
-        baseUrl: './lib/assets', // ibsheet 라이브러리 위치
-        locales: ['en','ko'],
-        plugins: [
-          'dialog',
-          'common',
-          'excel',
-        ]
-      }
-    ]
-  });
-
   return (
     <>
       <Layout />
--- samples/src/index.js
+++ samples/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
+import IBSheetLoader from '@ibsheet/loader';
+
+// 로더 config
+IBSheetLoader.config({
+  registry: [
+    {
+      name: 'ibsheet',
+      baseUrl: './lib/assets', // ibsheet 라이브러리 위치, '<publicpath>/ibsheet'
+      locales: ['en','ko'],
+      plugins: [
+        'dialog',
+        'common',
+        'excel',
+      ]
+    }
+  ]
+});
 
 ReactDOM.render(
   <React.StrictMode>
