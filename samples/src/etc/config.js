a5574d95a09db3aba8dfbf5f54f8e52c4cca1a60
이재호 <jhlee@ibleaders.co.kr>
2021-09-30 15:08:39 +0900

    update config

--- samples/src/etc/config.js
+++ samples/src/etc/config.js
@@ -1,29 +1,22 @@
 import IBSheetLoader from '@ibsheet/loader';
 
-const sheetjs = 'ibsheet';
-// ibsheet 라이브러리 위치, '<publicpath>/ibsheet'
-const url = 'https://demo.ibleaders.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/';
-const locales = ['en','ko'];
-const theme = 'material';
-const plugins = ['dialog', 'common', 'excel'];
+const ibsheetLib = {
+  name: 'ibsheet',
+  baseUrl: 'https://demo.ibleaders.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/',
+  locales: ['en', 'ko'],
+  theme: 'material',
+  plugins: ['excel', 'common', 'dialog']
+}
 
 // 로더 config
 IBSheetLoader.config({
-  registry: [
-    {
-      name: sheetjs,
-      baseUrl: url,
-      locales: locales,
-      theme: theme,
-      plugins: plugins
-    }
-  ]
+  registry: [ibsheetLib]
 });
 
 // 로더 이벤트
 IBSheetLoader.once('loaded', (evt) => {
   const target = evt.target;
-  if (target.alias === sheetjs) {
+  if (target.alias === ibsheetLib.name) {
     let IBSheet = IBSheetLoader.getIBSheetStatic();
     console.log('loaded', IBSheet);
   }
