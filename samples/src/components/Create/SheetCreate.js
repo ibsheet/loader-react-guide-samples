5a8c7da478dbb0b8ae16e0f9c99bbf07583fbae8
이재호 <jhlee@softin.co.kr>
2022-06-10 10:38:53 +0900

    Update: paging url

--- samples/src/components/Create/SheetCreate.js
+++ samples/src/components/Create/SheetCreate.js
@@ -103,7 +103,7 @@ const IBSheet8 = () => {
                 }
               } else if (evt.sheet.SearchMode === 3) {
                 const param = {
-                  url: 'http://localhost:8000/api/data',
+                  url: location.href.indexOf('localhost') > -1 ? 'http://localhost:8000/api/data' : 'https://dev.ibleaders.com/api/ibsheet/v8/samples/customer/paging.jsp',
                   // 카운팅 할 data 의 갯수와 전체 갯수를 param 으로 해서 보내도록 합니다.
                   param: 'data=100&total=20000&searchMode=' + evt.sheet.SearchMode,
                   callback: rtn => {
